0f7ce8cb2916bba2c7bdfe7655e2648c775bd39f
fahrlehrermitherz <info@fahrlehrermitherz.de>
2025-06-12 23:19:13 +0200

    Update routes.py

--- main/routes.py
+++ main/routes.py
@@ -1,9 +1,15 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, session, redirect, url_for
+from models import Schueler
+from datetime import date
 
-main = Blueprint('main', __name__)
+main_bp = Blueprint("main", __name__, template_folder="../templates/main")
 
-@main.route("/")
-@login_required
+@main_bp.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    if "user_id" not in session:
+        return redirect(url_for("auth.login"))
+
+    anzahl_schueler = Schueler.query.count()
+    heute = date.today().strftime("%d.%m.%Y")
+
+    return render_template("main/dashboard.html", anzahl_schueler=anzahl_schueler, heute=heute)
