c9ebb136b50c026f7c329677d7e90dbc155c9620
fahrlehrermitherz <info@fahrlehrermitherz.de>
2025-06-13 21:08:37 +0200

    Update routes.py

--- buero/routes.py
+++ buero/routes.py
@@ -1,34 +1,55 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
-from models import db, User, Fahrstundenprotokoll, Rolle
-from sqlalchemy import extract
+from models import db, FahrstundenTyp
 
-buero = Blueprint("buero", __name__, url_prefix="/buero")
+buero = Blueprint('buero', __name__, url_prefix='/buero')
 
-@buero.route("/verwaltung")
+@buero.route('/preise')
 @login_required
-def verwaltung():
-    if current_user.rolle.name != "Büro":
-        return "Zugriff verweigert", 403
+def preise_liste():
+    if current_user.rolle.name not in ['Büro', 'Superadmin']:
+        return "Nicht autorisiert", 403
+    typen = FahrstundenTyp.query.order_by(FahrstundenTyp.bezeichnung).all()
+    return render_template('buero/preise_liste.html', typen=typen)
 
-    monat = request.args.get("monat")
-    jahr = request.args.get("jahr")
+@buero.route('/preise/neu', methods=['GET', 'POST'])
+@login_required
+def preise_neu():
+    if current_user.rolle.name not in ['Büro', 'Superadmin']:
+        return "Nicht autorisiert", 403
+    if request.method == 'POST':
+        bezeichnung = request.form.get('bezeichnung')
+        minuten = request.form.get('minuten', type=int)
+        minutenpreis = request.form.get('minutenpreis', type=float)
+        neuer_typ = FahrstundenTyp(bezeichnung=bezeichnung, minuten=minuten, minutenpreis=minutenpreis)
+        db.session.add(neuer_typ)
+        db.session.commit()
+        flash('Fahrstunden-Typ hinzugefügt.', 'success')
+        return redirect(url_for('buero.preise_liste'))
+    return render_template('buero/preise_form.html', mode='neu')
 
-    query = Fahrstundenprotokoll.query.join(User, Fahrstundenprotokoll.fahrlehrer).order_by(
-        Fahrstundenprotokoll.datum.desc()
-    )
+@buero.route('/preise/edit/<int:typ_id>', methods=['GET', 'POST'])
+@login_required
+def preise_edit(typ_id):
+    if current_user.rolle.name not in ['Büro', 'Superadmin']:
+        return "Nicht autorisiert", 403
+    typ = FahrstundenTyp.query.get_or_404(typ_id)
+    if request.method == 'POST':
+        typ.bezeichnung = request.form.get('bezeichnung')
+        typ.minuten = request.form.get('minuten', type=int)
+        typ.minutenpreis = request.form.get('minutenpreis', type=float)
+        db.session.commit()
+        flash('Fahrstunden-Typ aktualisiert.', 'success')
+        return redirect(url_for('buero.preise_liste'))
+    return render_template('buero/preise_form.html', mode='edit', typ=typ)
 
-    if monat and jahr:
-        query = query.filter(
-            extract("month", Fahrstundenprotokoll.datum) == int(monat),
-            extract("year", Fahrstundenprotokoll.datum) == int(jahr)
-        )
-
-    fahrten = query.all()
-
-    # Fahrlehrer-Preise (dynamisch später aus Datenbank holen)
-    minutenpreise = {
-        "default": 1.20  # Fallback-Preis pro Minute
-    }
-
-    return render_template("buero/verwaltungsseite.html", fahrten=fahrten, minutenpreise=minutenpreise)
+@buero.route('/preise/delete/<int:typ_id>', methods=['POST'])
+@login_required
+def preise_delete(typ_id):
+    if current_user.rolle.name not in ['Büro', 'Superadmin']:
+        return "Nicht autorisiert", 403
+    typ = FahrstundenTyp.query.get_or_404(typ_id)
+    db.session.delete(typ)
+    db.session.commit()
+    flash('Fahrstunden-Typ gelöscht.', 'info')
+    return redirect(url_for('buero.preise_liste'))
