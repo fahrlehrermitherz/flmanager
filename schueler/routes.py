3035b5a4d7460ff08a3b143b603ce6ce4a3bb569
fahrlehrermitherz <info@fahrlehrermitherz.de>
2025-06-13 07:55:54 +0200

    Update routes.py

--- schueler/routes.py
+++ schueler/routes.py
@@ -1,10 +1,39 @@
-# schueler/routes.py
-from flask import Blueprint, render_template
-from models import db, Schueler
+from flask import Blueprint, render_template, request, redirect, url_for
+from models import db, Schueler, Fahrstundenprotokoll, FahrstundenTyp, User
+from flask_login import login_required, current_user
+from datetime import datetime
 
-schueler_bp = Blueprint('schueler', __name__, url_prefix="/schueler")
+schueler_bp = Blueprint('schueler', __name__)
 
-@schueler_bp.route("/")
-def schueler_liste():
-    alle_schueler = Schueler.query.order_by(Schueler.nachname).all()
-    return render_template("schueler/schueler_liste.html", schueler=alle_schueler)
+@schueler_bp.route('/fahrstunde/anlegen', methods=['GET', 'POST'])
+@login_required
+def fahrstunde_anlegen():
+    if request.method == 'POST':
+        schueler_id = request.form['schueler_id']
+        typ_id = request.form['typ_id']
+        datum = datetime.strptime(request.form['datum'], '%Y-%m-%d').date()
+        uhrzeit = datetime.strptime(request.form['uhrzeit'], '%H:%M').time()
+        dauer_minuten = int(request.form['dauer_minuten'])
+        inhalt = request.form['inhalt']
+        bezahlt = request.form['bezahlt']
+
+        neue_fahrstunde = Fahrstundenprotokoll(
+            schueler_id=schueler_id,
+            fahrlehrer_id=current_user.id,
+            typ_id=typ_id,
+            datum=datum,
+            uhrzeit=uhrzeit,
+            dauer_minuten=dauer_minuten,
+            inhalt=inhalt,
+            bezahlt=bezahlt
+        )
+
+        db.session.add(neue_fahrstunde)
+        db.session.commit()
+
+        return redirect(url_for('schueler.fahrstunde_anlegen'))
+
+    schueler = Schueler.query.all()
+    typen = FahrstundenTyp.query.all()
+
+    return render_template('schueler/fahrstunde_anlegen.html', schueler=schueler, typen=typen)
