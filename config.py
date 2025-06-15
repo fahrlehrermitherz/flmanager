7d552931af1f9d449aca379a7d203319df924241
fahrlehrermitherz <info@fahrlehrermitherz.de>
2025-06-15 22:41:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
     
     # Datenbank-URL: Public URL > DATABASE_URL > Fallback SQLite
-    SQLALCHEMY_DATABASE_URI = (
-        os.environ.get("postgresql://postgres:bydexzqxToeTpGxCynAynqxRRqBThlXd@nozomi.proxy.rlwy.net:37182/railway") or 
-        os.environ.get("postgresql://postgres:bydexzqxToeTpGxCynAynqxRRqBThlXd@postgres.railway.internal:5432/railway") or 
-        "sqlite:///local.db"
-    )
+ SQLALCHEMY_DATABASE_URI = (
+    os.environ.get("postgresql://postgres:bydexzqxToeTpGxCynAynqxRRqBThlXd@nozomi.proxy.rlwy.net:37182/railway") or 
+    os.environ.get("postgresql://postgres:bydexzqxToeTpGxCynAynqxRRqBThlXd@postgres.railway.internal:5432/railway") or 
+    "sqlite:///local.db"
+)
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
