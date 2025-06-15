95800bc8b8882983de342f7f7c275ce1babaaa3a
fahrlehrermitherz <info@fahrlehrermitherz.de>
2025-06-15 03:01:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,32 +3,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
 
-# Initialisierungen
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = "auth.login"
-
+login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'info'
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # Extensions initialisieren
     db.init_app(app)
     login_manager.init_app(app)
 
     # Blueprints importieren und registrieren
     from auth.routes import auth as auth_blueprint
-    from main.routes import calendar_bp as main_blueprint  # Hier der geänderte Import
-    from schueler.routes import schueler_bp as schueler_blueprint
+    from main.routes import main as main_blueprint
+    from buero.routes import buero as buero_blueprint
+    from schueler.routes import schueler as schueler_blueprint
 
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(main_blueprint)
-    app.register_blueprint(schueler_blueprint, url_prefix='/schueler')
+    app.register_blueprint(buero_blueprint)
+    app.register_blueprint(schueler_blueprint)
 
     return app
-
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
