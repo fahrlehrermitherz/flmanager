35e4a6ff446198f04a22e7f3c112111441d4f978
fahrlehrermitherz <info@fahrlehrermitherz.de>
2025-06-16 00:39:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
-import logging
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or "sqlite:///local.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
 
-    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgresql"):
+    if SQLALCHEMY_DATABASE_URI.startswith("postgresql"):
         SQLALCHEMY_ENGINE_OPTIONS = {
             "connect_args": {
                 "sslmode": "require"
@@ -14,7 +13,3 @@ class Config:
         }
     else:
         SQLALCHEMY_ENGINE_OPTIONS = {}
-
-# Logging für Debug-Zwecke
-logging.basicConfig(level=logging.INFO)
-logging.info(f"🔑 Using DB URI: {Config.SQLALCHEMY_DATABASE_URI}")
