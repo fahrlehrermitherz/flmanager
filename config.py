05eef94430961b648d1257cd11b0e1cbd1113055
fahrlehrermitherz <info@fahrlehrermitherz.de>
2025-06-16 00:29:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "sqlite:///local.db"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or "sqlite:///local.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
 
